3a6ce54882f7fdd2e81b9fbafb1b8ba1c2df5ccd
smith <smith@ieee.org>
2018-07-15 15:03:31 -0500

    first push
    * .gitignore: project created
    
    * staticLibWatchOSTest.sln:
    * Main.cs:
    * Info.plist:
    * AppDelegate.cs:
    * Main.storyboard:
    * ViewController.cs:
    * Entitlements.plist:
    * LaunchScreen.storyboard:
    * ViewController.designer.cs:
    * staticLibWatchOSTest.csproj:
    * Contents.json:
    * Info.plist:
    * Entitlements.plist:
    * Info.plist:
    * Interface.storyboard:
    * Contents.json:
    * testCKLib1.cs:
    * libtestCKLib1.a:
    * Entitlements.plist:
    * Contents.json:
    * ExtensionDelegate.cs:
    * InterfaceController.cs:
    * Contents.json:
    * InterfaceController.designer.cs:
    * Contents.json:
    * staticLibWatchOSTest.staticLibWatchOSTestWatch.csproj:
    * staticLibWatchOSTest.staticLibWatchOSTestWatchExtension.csproj:

---
+++ staticLibWatchOSTest.staticLibWatchOSTestWatchExtension/testCKLib1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace staticLibWatchOSTest.staticLibWatchOSTestWatchExtension
+{
+    public class testCKLib1
+    {
+        [DllImport("__Internal")]
+        public extern static int sum(int a, int b);
+    }
+}
